8fced5b37c4c4869f171205a3e4d6c68fbd278a6
Enes <enstasdemir@gmail.com>
2024-04-20 23:05:23 +0300

    Initial commit

---
+++ resources/lang/tr/passwords.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Parola Sıfırlama
+    |--------------------------------------------------------------------------
+    |
+    | Aşağıdaki metinler, başarısız olmuş bir parola sıfırlama
+    | işleminde oluşan hataları temsil eden mesajlardır.
+    |
+    */
+
+    'reset' => 'Parolanız sıfırlandı!',
+    'sent' => 'Parola sıfırlama bağlantınız e-posta ile gönderildi!',
+    'throttled' => 'Lütfen tekrar denemeden önce bekleyin.',
+    'token' => 'Parola sıfırlama adresi/kodu geçersiz.',
+    'user' => 'Bu e-posta adresi ile kayıtlı bir üye bulunmuyor.',
+	
+	// Pass reset page lang
+	'reset-title' => 'Parola Sıfırlama',
+	'reset-password' => 'Parolanızı Sıfırlayınız',
+	'email' => 'E-mail Adresiniz',
+	'sendtext' => 'Parolamı Sıfırla',
+	
+	
+
+];
